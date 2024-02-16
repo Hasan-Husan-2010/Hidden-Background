9c432ab5e8c98e77d47b090b92390d178a3ec1a2
HasanPro <naziramamatmurotova01@gmail.com>
2024-02-16 17:27:29 +0500

    Add2

--- Java.js
+++ Java.js
@@ -2,7 +2,7 @@ function setup(){
     createCanvas(windowWidth,windowHeight);
 }
 function draw(){
-    strokeWeight(10);
+    strokeWeight(15);
     if(mouseIsPressed === true){
         line(mouseX, mouseY, pmouseX, pmouseY)
     }
